b5b5128cb71e0aa8cc69e4fa99a5e22474119875
IvanIvanov6524 <ivanovivan_6524@mail.ru>
2021-07-05 14:57:57 +0300

    build: add navController

--- app/src/main/java/com/example/ivanov_p3/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/ivanov_p3/ui/fragment/SearchFragment.kt
@@ -1,16 +1,13 @@
 package com.example.ivanov_p3.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
 import com.example.ivanov_p3.R
 import com.example.ivanov_p3.common.base.BaseFragment
 import com.example.ivanov_p3.databinding.FragmentSearchBinding
+
 
 class SearchFragment : BaseFragment(R.layout.fragment_search) {
 
@@ -22,8 +19,6 @@ class SearchFragment : BaseFragment(R.layout.fragment_search) {
     ): View? {
         binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
 
-
-        binding.bottomNavigation.setupWithNavController(navController = NavController(requireContext()))
         return binding.root
     }
 
