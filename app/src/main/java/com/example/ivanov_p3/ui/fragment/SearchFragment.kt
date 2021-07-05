f4b2f10edfef4f4e5a5d373035dc99f303040aac
IvanIvanov6524 <ivanovivan_6524@mail.ru>
2021-07-05 13:23:37 +0300

    build: bottom menu

--- app/src/main/java/com/example/ivanov_p3/ui/fragment/SearchFragment.kt
+++ app/src/main/java/com/example/ivanov_p3/ui/fragment/SearchFragment.kt
@@ -5,16 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.ivanov_p3.R
+import com.example.ivanov_p3.common.base.BaseFragment
+import com.example.ivanov_p3.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment() {
+class SearchFragment : BaseFragment(R.layout.fragment_search) {
+
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+
+
+        binding.bottomNavigation.setupWithNavController(navController = NavController(requireContext()))
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/ivanov_p3/ui/fragment/SplashFragment.kt
+++ app/src/main/java/com/example/ivanov_p3/ui/fragment/SplashFragment.kt
@@ -5,19 +5,42 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.ivanov_p3.R
 import com.example.ivanov_p3.common.base.BaseFragment
 import com.example.ivanov_p3.databinding.FragmentSplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashFragment : BaseFragment(R.layout.fragment_splash) {
 
     private lateinit var binding: FragmentSplashBinding
+    private val SPLASH_TIME_OUT = 1500L
+    private val activityScope = CoroutineScope(Dispatchers.Main)
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentSplashBinding.inflate(layoutInflater, container, false)
+
+
+        activityScope.launch {
+            delay(SPLASH_TIME_OUT)
+
+//            if(prefs.state)
+                findNavController().navigate(R.id.action_splashFragment_to_searchFragment)
+//            else {
+//                val extras = FragmentNavigatorExtras(
+//                    binding.imageViewAppSplash to "imageTransition"
+//                )
+//                findNavController().navigate(R.id.action_splashFragment_to_authorisationFragment, null, null, extras)
+//            }
+        }
 
         return binding.root
     }
